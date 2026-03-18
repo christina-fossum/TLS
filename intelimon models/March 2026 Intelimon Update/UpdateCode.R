dc7ee194090bbd5697c103efb7bc3f04febf0cba
christina-fossum <christina_fossum@nps.gov>
2026-03-18 15:49:06 -0600

    total trees

--- intelimon models/March 2026 Intelimon Update/UpdateCode.R
+++ intelimon models/March 2026 Intelimon Update/UpdateCode.R
@@ -5,15 +5,15 @@
 # Look at pre/post/ yr-01 burn
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Pre-Burn
+# tree count
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 library(tidyverse)
 library(leaps)
 library(performance)
 library(ggplot2)
 
-pred <- read.csv("RMNP Intelimon Update/1. ROMO_East_Fuels/a. ROMO_East_Fuels_PRE_TotalAll/ROMO_East_Fuels_PRE_TotalAll_pred.csv")
-vars <- read.csv("RMNP Intelimon Update/1. ROMO_East_Fuels/a. ROMO_East_Fuels_PRE_TotalAll/ROMO_East_Fuels_PRE_TotalAll_vars.csv")
+pred <- read.csv("RMNP Intelimon Update/2. ROMO_East_Trees/a. ROMO_East_TotalTreeCount/ROMO_EAST_TotalTreeCount_pred.csv")
+vars <- read.csv("RMNP Intelimon Update/2. ROMO_East_Trees/a. ROMO_East_TotalTreeCount/ROMO_EAST_TotalTreeCount_vars.csv")
 vars <- vars %>% mutate(across(where(is.character), as.factor)) %>% select(where(~!is.factor(.) || nlevels(.) > 1)) %>% select(where(~sum(is.na(.)) == 0))
 
 
@@ -27,7 +27,7 @@ test_vars <- vars[test_indices, ]
 train_pred <- pred[-test_indices, ]
 test_pred <- pred[test_indices, ]
 
-regfit = regsubsets(train_pred$TotalAll~., train_vars,  nvmax = 5 , method = "seqrep", really.big = T) 
+regfit = regsubsets(train_pred$TreeCount~., train_vars,  nvmax = 5 , method = "seqrep", really.big = T) 
 
 # Visual checks to see how many variables you should use
 regsum<- summary(regfit)
@@ -42,21 +42,16 @@ vcov(regfit,2)
 coef(regfit,2)
 
 # 0.1 for test data
-# 2: MaxSD + hr100_1000_l1_median
-# 3: h_l2_median + MaxSD + hr100_1000_l1_median 
-# 4: h_l2_median + h_US_cnt+MaxSD + hr100_1000_l1_median 
-# 5: h_l2_median + h_US_cnt + MaxSD + hr100_1000_l1_median + hr100_1000_l1_tgi
+# 2: h_MS_cnt + fuel0_3l1_kurt  
+# 3:  h_MS_cnt + h_MS_per + fuel0_3l1_kurt 
+# 4: h_MS_cnt + h_zq20 + vox_l1_cnt + fuel0_3l1_kurt 
+# 5: h_l5_std + h_MS_cnt + h_zq20 + fDim + fuel0_3l1_kurt 
 
-# 0.05
-# 2: hr100_1000_l1_median + hr100_1000_l1_tgi 
-# 3: hr0_10_l1_vari + hr100_1000_l1_median + hr100_1000_l1_tgi 
-# 4: s_lng_prop_sk + hr0_10_l1_vari + hr100_1000_l1_median + hr100_1000_l1_tgi 
-# 5: h_l2_median + s_lng_prop_sk + hr0_10_l1_vari + hr100_1000_l1_median + hr100_1000_l1_tgi
 
 
 attach(train_vars)
 
-lm1 <- lm(formula = train_pred$TotalAll ~hr100_1000_l1_median + hr100_1000_l1_tgi )
+lm1 <- lm(formula = train_pred$TotalAll ~   h_ground_cnt + s_lng_zero_per + shrubs_l1_skew)
 
 
 
@@ -97,5 +92,5 @@ ggplot(data2,aes(x,y)) +
   theme(plot.title = element_text(hjust=0.5, size=20, face='bold') 
   ) 
 
-saveRDS(lm1, "RMNP Intelimon Update/1. ROMO_East_Fuels/a. ROMO_East_Fuels_PRE_TotalAll/ROMO_East_Fuels_PRE_TotalAll.rda")
+saveRDS(lm1, "RMNP Intelimon Update/1. ROMO_East_Fuels/b. ROMO_East_Fuels_POST_TotalAll/ROMO_East_Fuels_POST_TotalAll.rda")
 
